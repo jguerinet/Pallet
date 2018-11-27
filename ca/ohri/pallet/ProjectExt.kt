c351dd729746ed50ee1cba08dc599677036ce8fa
Julien Guerinet <julien@guerinet.com>
2018-11-27 14:05:52 -0500

    Add method to set up a project with its Vcs roots

--- ca/ohri/pallet/ProjectExt.kt
+++ ca/ohri/pallet/ProjectExt.kt
@@ -7,7 +7,20 @@ package ca.ohri.pallet
 import jetbrains.buildServer.configs.kotlin.v2018_1.Project
 import jetbrains.buildServer.configs.kotlin.v2018_1.RelativeId
 import jetbrains.buildServer.configs.kotlin.v2018_1.VcsRoot
+import jetbrains.buildServer.configs.kotlin.v2018_1.project
 import jetbrains.buildServer.configs.kotlin.v2018_1.vcs.GitVcsRoot
+
+/**
+ * Prepares a project by adding the 2 Vcs roots needed for GitFlow
+ */
+fun palletProject(url: String, gitKey: String, init: Project.() -> Unit) =
+    project {
+        init()
+
+        // Add the develop Vcs root and the releases/hotfixes Vcs Root
+        developVcsRoot(url, gitKey)
+        releasesHotfixesVcsRoot(url, gitKey)
+    }
 
 /**
  * Sets the order of the Build Types based on the order of the [ids]
@@ -19,14 +32,13 @@ fun Project.setOrder(vararg ids: String) {
 /**
  * Creates a Git Vcs root using the [id] as the Id and name, the Git [url], and the [branchSpec] to
  *  listen to. It uses the [branch] as the default branch (null if it's the Git default branch).
- *  The [authUsername] and [authPassword] are used to sign into Git
+ *  The [authPassword] is used to sign into Git
  */
 fun gitVcsRoot(
     id: String,
     url: String,
-    authUsername: String,
     authPassword: String,
-    branchSpec: String,
+    branchSpec: String? = null,
     branch: String? = null
 ): VcsRoot = GitVcsRoot {
     id(id)
@@ -35,26 +47,38 @@ fun gitVcsRoot(
     if (branch != null) {
         this.branch = Git.getHeadsRef(branch)
     }
-    this.branchSpec = branchSpec
+    if (branchSpec != null) {
+        this.branchSpec = branchSpec
+    }
     authMethod = password {
-        userName = authUsername
+        userName = Git.USERNAME
         password = authPassword
     }
     userNameStyle = GitVcsRoot.UserNameStyle.FULL
 }
 
 /**
- * Creates the Git Vcs root for the releases/hotfixes. Uses the Git [url], the [authPassword], and the [branch] to
- *  set this up. Calls [gitVcsRoot]
+ * Adds a Git Vcs root for the releases/hotfixes. Uses the Git [url] and the [authPassword] to set this up.
+ *  Calls [gitVcsRoot]
  */
-fun gitReleasesHotfixesVcsRoot(
+fun Project.releasesHotfixesVcsRoot(
     url: String,
-    authPassword: String,
-    branch: String? = null
-): VcsRoot = gitVcsRoot(
-    Id.RELEASES_HOTFIXES, url, Git.USERNAME, authPassword, """
+    authPassword: String
+) = vcsRoot(
+    gitVcsRoot(
+        Id.RELEASES_HOTFIXES,
+        url,
+        authPassword,
+        """
     ${Git.getHeadsRef(Git.RELEASES)}
     ${Git.getHeadsRef(Git.HOTFIXES)}
     """.trimIndent(),
-    branch
+        Git.MASTER
+    )
 )
+
+/**
+ * Adds a Git Vcs root for the develop branch. Uses the [url] and the [authPassword]. Calls [gitVcsRoot]
+ */
+fun Project.developVcsRoot(url: String, authPassword: String) =
+    vcsRoot(gitVcsRoot(Git.DEVELOP.capitalize(), url, authPassword, branch = Git.DEVELOP))
