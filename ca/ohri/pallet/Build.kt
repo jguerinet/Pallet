012e565e9bde68fb537191d621f209e2f9922cdd
Julien Guerinet <julien@guerinet.com>
2018-11-27 12:32:03 -0500

    Add the new PalletBuildType

--- ca/ohri/pallet/Build.kt
+++ ca/ohri/pallet/Build.kt
@@ -5,11 +5,7 @@
 package ca.ohri.pallet
 
 import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_1.DslContext
 import jetbrains.buildServer.configs.kotlin.v2018_1.Id
-import jetbrains.buildServer.configs.kotlin.v2018_1.Project
-import jetbrains.buildServer.configs.kotlin.v2018_1.buildFeatures.commitStatusPublisher
-import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
 
 /**
  * Different conventional build types
@@ -23,7 +19,7 @@ object Type {
 }
 
 /**
- * Creates a BuildType for the [title] (which is used as the Id and the name). The optional
+ * Creates a BuildType for the [buildType] (which is used as the Id and the name). The optional
  *  [shouldTriggerOnCommit] determines whether a new commit to a watched branhc should trigger a
  *  build. The optional [referenceName] is used to set up a branch trigger. [vcsRootId] tells us
  *  what Vcs root to use, defaults to the settings root. [shouldBuildDefaultBranch] tells us whether
@@ -33,83 +29,21 @@ object Type {
  *  (defaults to 50). The [init] block allows you to initialize other things, such as build
  *  features and steps.
  */
-fun Project.build(
-    title: String,
-    commitStatusPublisherKey: String,
+fun PalletProject.build(
+    buildType: String,
+    vcsRootId: Id,
     shouldTriggerOnCommit: Boolean = true,
     referenceName: String = "",
-    vcsRootId: Id = DslContext.settingsRoot.id!!,
     shouldBuildDefaultBranch: Boolean = false,
     artifactsPath: String? = null,
     artifactBuilds: Int = 10,
     historyBuilds: Int = 50,
-    init: BuildType.() -> Unit
-): BuildType {
-
-    return buildType {
-        // Set the Id and the name based on the title
-        id(title)
-        name = title
-
-        // Always only allow 1 running build
-        maxRunningBuilds = 1
-
-        // Always allow the external status widget
-        allowExternalStatus = true
-
-        // Set up the artifacts if we have it
-        if (artifactsPath != null) {
-            artifactRules = "+:$artifactsPath"
-        }
-
-        // Always add the version parameter, even if unused
-        params {
-            param("env.version", "0.0.0")
-        }
-
-        // Attack this VCS url
-        vcs {
-            root(vcsRootId)
-            DslContext.settingsRoot
-            buildDefaultBranch = shouldBuildDefaultBranch
-        }
-
-
-        triggers {
-            if (shouldTriggerOnCommit) {
-                vcs {
-                    // Always set up the Git trigger to not start if mHealthAdmin commits
-                    triggerRules = """
-                        -:user=mhealthadmin:**
-                        -:user=mhealthteam:**
-                        -:user=mhealthadmin <mhealthteam@toh.ca>:**
-                    """.trimIndent()
-
-                    branchFilter = referenceName
-                }
-            }
-        }
-
-        cleanup {
-            artifacts(builds = artifactBuilds)
-            history(builds = historyBuilds)
-        }
-
-        init()
-
-        // Set up the features from the base list and the settings
-        features {
-            commitStatusPublisher {
-                publisher = github {
-                    githubUrl = "https://api.github.com"
-                    authType = personalToken {
-                        token = commitStatusPublisherKey
-                    }
-                }
-            }
-        }
-    }
-}
+    init: PalletBuildType.() -> Unit
+): BuildType =
+    PalletBuildType(
+        buildType, gitKey, vcsRootId, shouldTriggerOnCommit, referenceName, shouldBuildDefaultBranch,
+        artifactsPath, artifactBuilds, historyBuilds, init
+    )
 
 /**
  * Creates and returns the reference to the branch with [branchName]
---
+++ ca/ohri/pallet/PalletBuildType.kt
@@ -0,0 +1,83 @@
+package ca.ohri.pallet
+
+import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
+import jetbrains.buildServer.configs.kotlin.v2018_1.Id
+import jetbrains.buildServer.configs.kotlin.v2018_1.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.vcs
+
+class PalletBuildType(
+    val buildType: String,
+    gitKey: String,
+    vcsRootId: Id,
+    shouldTriggerOnCommit: Boolean = true,
+    referenceName: String = "",
+    shouldBuildDefaultBranch: Boolean = false,
+    artifactsPath: String? = null,
+    artifactBuilds: Int = 10,
+    historyBuilds: Int = 50,
+    init: PalletBuildType.() -> Unit
+) : BuildType() {
+
+    init {
+        // Set the Id and the name based on the title
+        id(buildType)
+        name = buildType
+
+        // Always only allow 1 running build
+        maxRunningBuilds = 1
+
+        // Always allow the external status widget
+        allowExternalStatus = true
+
+        // Set up the artifacts if we have it
+        if (artifactsPath != null) {
+            artifactRules = "+:$artifactsPath"
+        }
+
+        // Always add the version parameter, even if unused
+        params {
+            param("env.version", "0.0.0")
+        }
+
+        // Attack this VCS url
+        vcs {
+            root(vcsRootId)
+            buildDefaultBranch = shouldBuildDefaultBranch
+        }
+
+
+        triggers {
+            if (shouldTriggerOnCommit) {
+                vcs {
+                    // Always set up the Git trigger to not start if mHealthAdmin commits
+                    triggerRules = """
+                        -:user=mhealthadmin:**
+                        -:user=mhealthteam:**
+                        -:user=mhealthadmin <mhealthteam@toh.ca>:**
+                    """.trimIndent()
+
+                    branchFilter = referenceName
+                }
+            }
+        }
+
+        cleanup {
+            artifacts(builds = artifactBuilds)
+            history(builds = historyBuilds)
+        }
+
+        init()
+
+        // Set up the features from the base list and the settings
+        features {
+            commitStatusPublisher {
+                publisher = github {
+                    githubUrl = "https://api.github.com"
+                    authType = personalToken {
+                        token = gitKey
+                    }
+                }
+            }
+        }
+    }
+}
