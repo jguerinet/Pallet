5f8c80bc5ba53acd0fff6712d1ae890af3b9d681
Julien Guerinet <julien@guerinet.com>
2019-05-16 17:09:28 -0400

    Convert project to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018-2019 Julien Guerinet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Version {
+    const val kotlin = "1.3.31"
+    const val teamCityDsl = "2018.2"
+    const val teamCityPlugins = "1.0-SNAPSHOT"
+}
+
+plugins {
+    java
+    id("com.github.ben-manes.versions") version "0.21.0"
+}
+
+apply(from = "https://raw.githubusercontent.com/jguerinet/Gradle-Artifact-Scripts/master/spotless.gradle")
+
+group = "com.guerinet"
+version = "5.0.0"
+
+buildscript {
+
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:3.23.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.21.0")
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("http://download.jetbrains.com/teamcity-repository")
+    maven("https://teamcity.jetbrains.com/app/dsl-plugins-repository")
+    maven("https://teamcity.internal.company.com/app/dsl-plugins-repository")
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Version.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-script-runtime:${Version.kotlin}")
+    implementation("org.jetbrains.teamcity:configs-dsl-kotlin:${Version.teamCityDsl}")
+    implementation("org.jetbrains.teamcity:configs-dsl-kotlin-bundled:${Version.teamCityPlugins}")
+    implementation("org.jetbrains.teamcity:configs-dsl-kotlin-commandLineRunner:${Version.teamCityPlugins}")
+    implementation("org.jetbrains.teamcity:configs-dsl-kotlin-commit-status-publisher:${Version.teamCityPlugins}")
+    implementation("org.jetbrains.teamcity:configs-dsl-kotlin-gradle:${Version.teamCityPlugins}")
+    implementation("org.jetbrains.teamcity:configs-dsl-kotlin-jetbrains.git:${Version.teamCityPlugins}")
+    implementation("org.jetbrains.teamcity:configs-dsl-kotlin-pull-requests:${Version.teamCityPlugins}")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
