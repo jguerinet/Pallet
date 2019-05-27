bba3dc8df7a81d4c07e6572889793c3627d6e70d
Julien Guerinet <julien@guerinet.com>
2019-05-27 17:23:01 -0400

    Update how Kotlin dependencies were referenced

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,23 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (c) 2018-2019 Julien Guerinet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 object Version {
-    const val kotlin = "1.3.31"
     const val teamCityDsl = "2018.2"
     const val teamCityPlugins = "1.0-SNAPSHOT"
 }
@@ -68,8 +83,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Version.kotlin}")
-    implementation("org.jetbrains.kotlin:kotlin-script-runtime:${Version.kotlin}")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("script-runtime"))
     implementation("org.jetbrains.teamcity:configs-dsl-kotlin:${Version.teamCityDsl}")
     implementation("org.jetbrains.teamcity:configs-dsl-kotlin-bundled:${Version.teamCityPlugins}")
     implementation("org.jetbrains.teamcity:configs-dsl-kotlin-commandLineRunner:${Version.teamCityPlugins}")
